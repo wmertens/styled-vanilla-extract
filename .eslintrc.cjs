b9674a556f3259f72d17ae3b57990a2e76bd897d
Wout Mertens <Wout.Mertens@gmail.com>
2022-11-21 16:40:36 +0100

    build on release

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
+		project: ['./tsconfig.eslint.json'],
 		ecmaVersion: 2021,
 		sourceType: 'module',
 		ecmaFeatures: {
