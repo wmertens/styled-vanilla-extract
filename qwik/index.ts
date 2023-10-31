3c145a450f2d0b737fcead7a15ef781d4e5964c3
Wout Mertens <Wout.Mertens@gmail.com>
2023-10-31 14:10:42 +0100

    fix ts imports for real this time

--- qwik/index.ts
+++ qwik/index.ts
@@ -1,4 +1,2 @@
 // re-export to make TS happy when not using nodenext import resolution
-export type * from '../lib/index.d.ts'
-// @ts-expect-error 2308
-export * from '../lib/index.mjs'
+export * from '../lib'
--- vite/index.ts
+++ vite/index.ts
@@ -1,4 +1,2 @@
 // re-export to make TS happy when not using nodenext import resolution
-export type * from '../lib/vite.d.ts'
-// @ts-expect-error 2308
-export * from '../lib/vite.mjs'
+export * from '../lib/vite'
